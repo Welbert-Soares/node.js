088820d305b05a99009ca9b061aea86213cc71a3
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-01 15:12:39 -0300

    modulos

---
+++ incio/app.js
@@ -0,0 +1,9 @@
+const calculadora = require("./calculadora");
+
+
+console.log(calculadora.nome); 
+console.log(calculadora.soma(1, 2));
+console.log(calculadora.subtracao(1, 2));
+console.log(calculadora.multiplicacao(1, 2));
+console.log(calculadora.divisao(1, 2));
+
