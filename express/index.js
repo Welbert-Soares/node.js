de20c8e8000bf43e0fac7144cf05bd86904ae676
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-01 16:06:08 -0300

    rotas

--- express/index.js
+++ express/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+
+app.get("/", (req, res) => {
+    res.send("Ola meu chapa");
+});
+
+app.get("/blog", (req, res) => {
+    res.send("Bem vindo ao meu blog")
+});
+
+app.get("/perfil/opcoes", (req, res) => {
+    res.send("Aqui você pode editar seu perfil");
+});
+
+
+
 app.listen(3000, (error) => {
   if (error) {
     console.log("Error starting the server");
